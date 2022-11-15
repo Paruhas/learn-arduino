bb9abf7b29db10e65d52ff0a9b6d6f7e08a40797
LENOVO-Y530\BANK <bayulgang@hotmail.com>
2022-11-15 14:41:50 +0700

    edit connect WIFI

--- Connect_WIFI/Connect_WIFI.ino
+++ Connect_WIFI/Connect_WIFI.ino
@@ -1,47 +1,60 @@
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
+
 // const char* ssid     = "Your WiFi SSID";
 // const char* password = "Your Password";
 const char* ssid     = "MINERTA242789";
 const char* password = "Mit242789";
 
 void setup () {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT); // SET UP LED_BUILTIN
+  digitalWrite(LED_BUILTIN, 1); // off LED when start
+  
+  delay(2000); // delay start
 
-  Serial.begin(9600); //
-  WiFi.begin(ssid, password); //ทำการ Connect SSID และ Pass
+  if (!SPIFFS.begin())  {
+    Serial.println("=== An error has occurred whiel mounting SPIFFS!. ===");
+    return;
+  }
+  
+  WiFi.disconnect(); // disconnect ก่อน เพื่อความชัว
+  delay(100);
+  WiFi.begin(ssid, password); // ทำการ Connect SSID และ Pass
   
   while (WiFi.status() != WL_CONNECTED) { // ถ้าไม่สามารถเชื่อมต่อได้
-    // ทำการ Print ทุก 1000ms
+    digitalWrite(LED_BUILTIN, 0);
+    delay(250);
+    digitalWrite(LED_BUILTIN, 1);
+    delay(250);
+
     Serial.print("Attempting to connect to Network named: ");  
     Serial.print(ssid);
     Serial.printf(" =>> Connection Status: %d \n", WiFi.status()); 
-    // แสดงสถานะการเชื่อมต่อ
-    delay(1000);
+
+    digitalWrite(LED_BUILTIN, 0);
+    delay(250);
+    digitalWrite(LED_BUILTIN, 1);
+    delay(250);
   }
   // จะหลุดออกจาก while ก็ต่อเมือ Connected เรียบร้อย
+  digitalWrite(LED_BUILTIN, 0); // on LED when complete
+
   Serial.println(" ========== ");
   Serial.print("Wi-Fi "); 
   Serial.print(ssid); 
-  Serial.println(" has been connected."); 
+  Serial.print(" has been connected, "); 
   Serial.print("at IP Address : "); 
   Serial.println(WiFi.localIP());  // ทำการ Print IP ที่ได้รับมาจาก 
   Serial.println(" ========== ");
 }
 
 void loop () { 
-  if (WiFi.status() == 3) 
-  {
+  if (WiFi.status() != 3) {
     digitalWrite(LED_BUILTIN, 1);
-    Serial.printf("> Current connection Status: %d\n", WiFi.status());// แสดงสถานะการเชื่อมต่อ
-    
-    digitalWrite(LED_BUILTIN, 0);
-    delay(1);
-    digitalWrite(LED_BUILTIN, 1);
-    delay(1);
-  } else 
-  {
-    digitalWrite(LED_BUILTIN, 0);
-  }  
+    Serial.printf("WiFi disconnect > connection Status: %d\n", WiFi.status());// แสดงสถานะการเชื่อมต่อ
+  } 
 
   delay(2000);
 }
